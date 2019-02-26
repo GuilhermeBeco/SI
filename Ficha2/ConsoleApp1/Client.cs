5a83fdb914440720c15b65ac16151727b162e3b7
ITzUndeeAd <44602509+ITzUndeeAd@users.noreply.github.com>
2019-02-26 18:40:41 +0000

    kek
    cesar com erro

--- Ficha2/ConsoleApp1/Client.cs
+++ Ficha2/ConsoleApp1/Client.cs
@@ -12,21 +12,49 @@ namespace ConsoleApp1
     {
         private const int PORT = 9999;
         private const int MAX_BUFFER_SIZE = 1024;
+        
+
         static void Main(string[] args)
         {
             TcpClient client = null;
+            Random rnd = new Random();
             NetworkStream stream = null;
             byte[] buffer = null;
             int bytesRead = 0;
+            int keyAux = 0;
+            int resMsg = 0;
+              
             try
             {
+               string msg = "EI SI";
                 client = new TcpClient();
                 client.Connect(IPAddress.Loopback, PORT);
                 stream = client.GetStream();
                 buffer = new byte[MAX_BUFFER_SIZE];
                 //------------------------------
                 #region Client comm block
-                   string msg = "EI SI";
+                   //string msg = "EI SI";
+                   int key = rnd.Next(1, 26);
+                   for(int i = 0; i< msg.Length; i++)
+                    {
+                    resMsg =(int)msg[i];
+                    if (resMsg != 32)
+                    {
+                        if (resMsg + key <= 90)
+                        {
+                            resMsg += key;
+
+                        }
+                        else
+                        {
+                            keyAux = 90 - resMsg;
+                            resMsg += keyAux;
+
+                        }
+                    }
+                                       
+                    }
+                   
                    byte[] packet = Encoding.UTF8.GetBytes(msg);
                    stream.Write(packet, 0, packet.Length);
                    Console.WriteLine("Sent data to server");
@@ -66,4 +94,5 @@ namespace ConsoleApp1
             Console.ReadKey();
         }
     }
+   
 }
