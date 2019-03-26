58f5ec472ae7f5a421e9cd0b29771ee04979965f
ITzUndeeAd <44602509+ITzUndeeAd@users.noreply.github.com>
2019-03-26 09:35:01 +0000

    ws5

---
+++ ei.si-worksheet5-ex1.1/ei.si-worksheet5-ex1.1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ei.si.worksheet5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonMD5ComputeHash_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSHA1ComputeHash_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSHA256ComputeHash_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSHA512ComputeHash_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
